ffbff62f23052269e13b7dded1c2caf0c8b580e4
Boyan Levchev <boyanlevchev@gmail.com>
2020-03-02 15:37:36 +0000

    did sum bits

---
+++ db/migrate/20200302153032_create_spaces.rb
@@ -0,0 +1,11 @@
+class CreateSpaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spaces do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
