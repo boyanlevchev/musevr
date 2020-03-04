4935c0b61b3405a4605cdd993959059ac4e33084
Kate Shin <kzzzsy111@gmail.com>
2020-03-04 15:59:34 +0000

    index page in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :show_navbar
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :bio, :username])
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:bio, :photo, :username])
+  end
+
+  def show_navbar
+    @show_navbar = true
   end
 end
