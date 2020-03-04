801753f0410dd5d599d23980a79137a019e9a4e3
Kate Shin <kzzzsy111@gmail.com>
2020-03-04 17:32:50 +0000

    artist index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
-  skip_before_action :show_navbar, only: :index
+
 
   def show
     @user = User.find(params[:id])
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @spaces = []
-    @users.each do |user|
-      space = Space.where('user_id = ?', user.id).first
-      @spaces << space
-    end
+    # @spaces = []
+    # @users.each do |user|
+    #   space = Space.where('user_id = ?', user.id).first
+    #   @spaces << space
+    # end
 
   end
 end
