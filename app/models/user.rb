2983add6569c67f844f201dfb4ec196bb093b012
Kate Shin <kzzzsy111@gmail.com>
2020-03-05 15:13:45 +0000

    assigned random colors for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :username, presence: true, uniqueness: true
+  before_create :set_color_to_random_value
+
+  COLORS = ['#fce2db', '#DE7800', '#FFFFDD']
+
+  private
+
+  def set_color_to_random_value
+    if User.all.empty?
+      self.color = COLORS.sample
+    else
+      self.color = COLORS.reject {|color| color == User.last.color}.sample
+    end
+  end
 end
