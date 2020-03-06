84a22825a4b50b071929341e13a5ab8dae336682
Kate Shin <kzzzsy111@gmail.com>
2020-03-06 16:10:35 +0000

    fix design of space show page

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :vr]
   before_action :set_space, only: [:show, :edit, :destoy, :update]
 
   def index
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,25 @@ require('aframe-mouse-dragndrop-component');
 
 import "bootstrap";
 
-// document.addEventListener('turbolinks:load', () => {
-//   //add all JS functions here!!
+document.addEventListener('turbolinks:load', () => {
+  //add all JS functions here!!
+
+//   AFRAME.registerComponent('cursor-listener', {
+//     init: function () {
+//            this.el.addEventListener('click', function (evt) {
+//             console.log('I was clicked');
+//         });
+//   }
 // });
+//   AFRAME.registerComponent('raycaster-autorefresh', {
+//   init: function () {
+//     var el = this.el;
+//     this.el.addEventListener('model-loaded', function () {
+//       var cursorEl = el.querySelector('[raycaster]');
+//       cursorEl.components.raycaster.refreshObjects();
+//     });
+//   }
+// });
+//   });
+
+});
