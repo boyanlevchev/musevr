d44d80674edcb8abc24fa0d5078a222355481159
Kate Shin <kzzzsy111@gmail.com>
2020-03-11 11:56:13 +0000

    vr pop up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,31 +1,28 @@
 require("@rails/ujs").start()
+require('turbolinks').start()
 require("@rails/activestorage").start()
 require("channels")
 require('aframe');
 // require('aframe-event-set-component');
 require('aframe-mouse-dragndrop-component');
+require('@editvr/aframe-dialog-popup-component');
+require('aframe-look-at-component');
 
 import "bootstrap";
+import { scaleControl } from '../components/a_frame_components';
+import { rotationControl } from '../components/a_frame_components';
+import { showPanel } from '../components/a_frame_components';
+// document.addEventListener('turbolinks:load', () => {
+//   //add all JS functions here!!
+
+// });
 
 document.addEventListener('turbolinks:load', () => {
-  //add all JS functions here!!
 
-//   AFRAME.registerComponent('cursor-listener', {
-//     init: function () {
-//            this.el.addEventListener('click', function (evt) {
-//             console.log('I was clicked');
-//         });
-//   }
-// });
-//   AFRAME.registerComponent('raycaster-autorefresh', {
-//   init: function () {
-//     var el = this.el;
-//     this.el.addEventListener('model-loaded', function () {
-//       var cursorEl = el.querySelector('[raycaster]');
-//       cursorEl.components.raycaster.refreshObjects();
-//     });
-//   }
-// });
-//   });
+  scaleControl();
+  rotationControl();
+  showPanel();
 
 });
+
+
